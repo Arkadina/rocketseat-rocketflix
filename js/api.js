b1e86986517fbb62a492023a7246bf1fb5fa2fcd
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-02-12 11:10:22 -0300

    added js

---
+++ js/api.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://api.themoviedb.org/3/trending/movie/week";
+export const IMG_URL = "https://image.tmdb.org/t/p/w500";
+export const language = "language=pt-BR";
--- js/index.js
+++ js/index.js
@@ -1,5 +1,40 @@
 const btn = document.getElementById("btn");
+const movieContainer = document.getElementById("movieContainer");
+const paragraph = document.querySelector("#movieContainer p");
+const h2 = document.querySelector("#movieContainer h2");
+const img = document.querySelector("#movieContainer img ");
 
-btn.addEventListener("click", () => {
-    alert("click");
+import { BASE_URL, API_KEY, IMG_URL, language } from "./api.js";
+
+btn.addEventListener("click", async () => {
+    let data = await getData();
+
+    const number = generateNumber(data.results.length);
+
+    movieContainer.classList.remove("hide");
+    movieContainer.classList.add("movie");
+
+    console.log(data.results[number]);
+
+    h2.textContent = data.results[number].original_title;
+    paragraph.textContent = data.results[number].overview;
+    img.setAttribute("src", `${IMG_URL}/${data.results[number].backdrop_path}`);
 });
+
+function generateNumber(length) {
+    const number = Math.floor(Math.random() * length);
+
+    return number;
+}
+
+async function getData() {
+    let data;
+    await axios
+        .get(`${BASE_URL}?api_key=${API_KEY}&${language}`)
+        .then((res) => {
+            data = res.data;
+            console.log(data);
+        });
+
+    return data;
+}
